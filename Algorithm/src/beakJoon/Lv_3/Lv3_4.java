26550c9e20ece9c55cecdbdfaaf38b956e4dab70
seop0728 <ms_sh0728@naver.com>
2022-08-21 20:57:00 +0900

    IntelliJ_Algorithm

---
+++ Algorithm/src/beakJoon/Lv_3/Lv3_4.java
@@ -0,0 +1,30 @@
+package beakJoon.Lv_3;
+
+import java.util.Scanner;
+
+//구매한 물건의 가격과 개수로 계산한 총 금액이 영수증에 적힌
+//총 금액과 일치하면 Yes를 출력한다. 일치하지 않는다면 No를 출력한다.
+public class Lv3_4 {
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        int total =  sc.nextInt();
+        int count = sc.nextInt();
+
+        for(int i = 0; i< count; i++){
+            int price = sc.nextInt();
+            int num = sc.nextInt();
+
+            total = total - (price * num);
+        }
+
+        if(total == 0) System.out.println("Yes");
+        else System.out.println("No");
+
+
+    }
+	}
+
